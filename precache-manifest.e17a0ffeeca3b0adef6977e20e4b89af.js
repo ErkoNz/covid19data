df4ad58cf8187b18324f2b97439496afec55ab41
ErkoN Z <erik.zabransky@gmail.com>
2020-09-26 20:43:14 +0200

    Updates

---
+++ precache-manifest.e17a0ffeeca3b0adef6977e20e4b89af.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf0186a8ad93014cdd11c46fecc034ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0df08292600078589ffd",
+    "url": "/static/css/main.69e03bbf.chunk.css"
+  },
+  {
+    "revision": "182893769f5eff537e74",
+    "url": "/static/js/2.ff8592e3.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/static/js/2.ff8592e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0df08292600078589ffd",
+    "url": "/static/js/main.1f757331.chunk.js"
+  },
+  {
+    "revision": "9ee5787c3cf2a74872ca",
+    "url": "/static/js/runtime-main.da7c08d4.js"
+  },
+  {
+    "revision": "cdc4522b9dca0855e5f4abb9920d7dc8",
+    "url": "/static/media/earth.cdc4522b.svg"
+  },
+  {
+    "revision": "a29682ddc11eeea095c712775ad8ab65",
+    "url": "/static/media/slovakiaIcon.a29682dd.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/production4/precache-manifest.8c65ff1d11b3a2d0c69decd29ae6408d.js"
+  "/precache-manifest.e17a0ffeeca3b0adef6977e20e4b89af.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/production4/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
